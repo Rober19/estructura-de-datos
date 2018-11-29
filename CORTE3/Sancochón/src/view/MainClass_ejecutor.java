b7334150f625cef3de9c7a6d094be16b2d4637f8
Rober19 <texas_telecaster@gmail.com>
2018-11-29 08:14:20 -0500

    v2

---
+++ CORTE3/Sancochón/src/view/MainClass_ejecutor.java
@@ -0,0 +1,11 @@
+
+package view;
+
+import view.VentanaArista;
+import view.VentanaPrincipal;
+
+public class MainClass_ejecutor {
+    public static void main(String[] args) {
+        new VentanaPrincipal().setVisible(true);
+ }
+}
