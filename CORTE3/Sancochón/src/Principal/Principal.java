7e56685777b00532a0aee0e118e044fa7a506cf0
Rober19 <texas_telecaster@gmail.com>
2018-11-28 20:18:03 -0500

    v2

---
+++ CORTE3/Sancochón/src/Principal/Principal.java
@@ -0,0 +1,11 @@
+
+package Principal;
+
+import Ventanas.VentanaArista;
+import Ventanas.VentanaPrincipal;
+
+public class Principal {
+    public static void main(String[] args) {
+        new VentanaPrincipal().setVisible(true);
+ }
+}
